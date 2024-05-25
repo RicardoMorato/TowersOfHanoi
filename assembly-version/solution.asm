3d8ad7a838d7569fa4d5032b3c71b4f89a0904c6
Ricardo Morato <rmr@cin.ufpe.br>
2024-05-25 19:27:33 -0300

    fix: add old code back

--- assembly-version/solution.asm
+++ assembly-version/solution.asm
@@ -16,6 +16,9 @@ section .data
     to_msg db " to ", 0
     newline db 10
 
+    error_msg db "Error: Invalid number entered. The number of disks must be > 0", LF, 0; mensagem de erro, a deve ser um numero positivo
+    error_len equ $-error_msg
+
     msg_test db "KKKKKKKKK ", LF ; mensagem a ser exibida
     len equ $-msg_test ; tamanho da mensagem
 
@@ -99,3 +102,51 @@ exit:
     mov eax, 1  ; sys_exit
     xor ebx, ebx
     int 0x80
+
+; Função para verificar e converter a entrada de string para inteiro
+converter_valor:
+    lea esi, [num_disks]
+    mov ecx, 2 ; tamanho máximo da entrada
+    call string_to_int
+    test eax, eax
+    js invalid_number ; se eax < 0, é um número inválido
+    ret
+
+invalid_number:
+    ; Exibir a mensagem de erro
+    mov eax, sys_write
+    mov ebx, STDOUT
+    mov ecx, error_msg
+    mov edx, error_len
+    int sys_call
+
+    ; Finalizar o programa com erro
+    mov eax, sys_exit
+    mov ebx, 1 ; código de erro
+    int sys_call
+
+; Função de conversão de string para inteiro
+string_to_int:
+    xor ebx, ebx ; limpar ebx para o acumulador
+    xor eax, eax ; limpar eax para o valor de retorno
+.prox_digito:
+    movzx eax, byte [esi]
+    inc esi
+    cmp al, LF
+    je end_convert
+    cmp al, '0'
+    jbe invalid_input ; se o caractere for menor ou igual a '0', é inválido
+    sub al, '0'
+    imul ebx, ebx, 10
+    add ebx, eax ; ebx = ebx*10 + eax
+    loop .prox_digito ; while (--ecx)
+    mov eax, ebx
+    ret
+
+invalid_input:
+    mov eax, -1 ; retornar -1 para indicar erro
+    ret
+
+end_convert:
+    mov eax, ebx
+    ret
