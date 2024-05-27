46040ab8bb3f3c94581d382489f9435f04336b60
Ricardo Morato <rmr@cin.ufpe.br>
2024-05-26 22:50:45 -0300

    fix: print procedure and comments

--- assembly-version/solution.asm
+++ assembly-version/solution.asm
@@ -1,6 +1,6 @@
-segment .data ; segmento de dados
+section .data
     ;renomeação do das operações utilizadas pra ficar mais fácil de chamar
-    LF          equ 0xA ; Quebra de linha
+    EOL         equ 0xA ; Quebra de linha
     NULL        equ 0xD ; Final de linha
     sys_call    equ 0x80 ; interrupção do sistema e envio de informações
     ret_exit    equ 0x0 ; operação de finalização
@@ -10,106 +10,64 @@ segment .data ; segmento de dados
     STDIN       equ 0x0 ; entrada padrão
     STDOUT      equ 0x1 ; saída padrão
 
-section .data
-    msg db "Move disk ", 0
-    from_msg db " from ", 0
-    to_msg db " to ", 0
-    newline db 10
+    ; Mensagem de input de quantidade de discos
+    msg_disk db "Enter the number of disks for the problem: "
+    len equ $-msg_disk
 
-    error_msg db "Error: Invalid number entered. The number of disks must be > 0", LF, 0; mensagem de erro, a deve ser um numero positivo
+    ; Mensagem de erro, o número de discos deve ser um numero positivo e maior que 0
+    error_msg db "Error: Invalid number entered. The number of disks must be > 0", EOL, 0
     error_len equ $-error_msg
 
-    msg_test db "KKKKKKKKK ", LF ; mensagem a ser exibida
-    len equ $-msg_test ; tamanho da mensagem
+    ; Mensagem para exibir o movimento dos discos
+    msg:                db "Move disk "
+    disk:               db  " "           ; disco a ser movido
+                        db " from "       ; de
+    tower_source:       db " "            ; torre de saída
+                        db " to "         ; para
+    tower_destination:  db " ", EOL       ; torre de destino
+    output_length       equ $-msg
 
 section .bss
-    digit resb 4  ; Buffer para armazenar o número do disco como string
-    disk_number resb 4
-    source resb 1
-    destination resb 1
-    auxiliar resb 1
+    num_disks resb 3 ; buffer para a entrada do número de discos
+    num_disks_int resd 1 ; armazenamento do número de discos como inteiro
 
 section .text
     global _start
 
 _start:
-    ; Chamada inicial para a função tower_of_hanoi
-    mov dword [disk_number], 3       ; Defina o número de discos
-    mov byte [source], 'A'
-    mov byte [destination], 'C'
-    mov byte [auxiliar], 'B'
-
-    mov edx, [disk_number]
-    push edx ; Salva valor de disk_number na pilha
-
-    movzx eax, byte [source]
-    movzx ecx, byte [destination]
-    movzx ebx, byte [auxiliar]
-    call tower_of_hanoi
-    call exit
-
-tower_of_hanoi:
-    push ebp
-    mov ebp, esp
-
-    ; Parâmetros: (disk_number, source, destination, auxiliar)
-    ; [edx] = disk_number
-    ; [eax] = source
-    ; [ecx] = destination
-    ; [ebx] = auxiliar
-
-    ; Caso base: if disk_number <= 0
-    cmp edx, 0
-    jle end_recursion
-
-    ; Recursively call tower_of_hanoi(disk_number - 1, source, auxiliar, destination)
-    dec edx
-    movzx eax, byte [source]
-    movzx ecx, byte [destination]
-    movzx ebx, byte [auxiliar]
-    call tower_of_hanoi
-
-    ; Print "MOVE DISK N TO Y FROM Z" -> Não tá rolando, tá printando a msg_test
+    ; Exibir a mensagem para o usuário
     mov eax, sys_write
     mov ebx, STDOUT
-    mov ecx, msg_test
+    mov ecx, msg_disk
     mov edx, len
     int sys_call
 
-    call atualiza_num_disks
+    ; Ler a entrada do usuário
+    mov eax, sys_read
+    mov ebx, STDIN
+    mov ecx, num_disks
+    mov edx, 2                     ; tamanho da entrada (ler até 2 algarismos)
+    int sys_call
 
-    movzx eax, byte [source]
-    movzx ecx, byte [destination]
-    movzx ebx, byte [auxiliar]
+    ; Verificar e converter a string para inteiro
+    call converter_valor
+
+    ; Empurrando as torres e a quantidade de discos na pilha de referencia para que fiquem em ordem
+    push dword 2                  ; Torre Auxiliar
+    push dword 3                  ; Torre Destino
+    push dword 1                  ; Torre Origem
+    push eax                      ; Número de discos inserido pelo usuário
+
     call tower_of_hanoi
-
-atualiza_num_disks:
-    pop edx
-    dec edx
-
-    cmp edx, 0
-    jge .push_edx
-
-    .push_edx:
-        push edx
-
-end_recursion:
-    mov esp, ebp
-    pop ebp
-    ret
-
-exit:
-    mov eax, 1  ; sys_exit
-    xor ebx, ebx
-    int 0x80
+    call exit
 
 ; Função para verificar e converter a entrada de string para inteiro
 converter_valor:
-    lea esi, [disk_number]
+    lea esi, [num_disks]
     mov ecx, 2 ; tamanho máximo da entrada
     call string_to_int
-    test eax, eax
-    js invalid_number ; se eax < 0, é um número inválido
+    cmp eax, 0
+    jl invalid_number ; se eax < 0, é um número inválido
     ret
 
 invalid_number:
@@ -129,13 +87,14 @@ invalid_number:
 string_to_int:
     xor ebx, ebx ; limpar ebx para o acumulador
     xor eax, eax ; limpar eax para o valor de retorno
+
 .prox_digito:
     movzx eax, byte [esi]
     inc esi
-    cmp al, LF
+    cmp al, EOL
     je end_convert
     cmp al, '0'
-    jbe invalid_input ; se o caractere for menor ou igual a '0', é inválido
+    jb invalid_input ; se o caractere for menor que '0', é inválido
     sub al, '0'
     imul ebx, ebx, 10
     add ebx, eax ; ebx = ebx*10 + eax
@@ -150,3 +109,80 @@ invalid_input:
 end_convert:
     mov eax, ebx
     ret
+
+tower_of_hanoi:
+    ; Parâmetros: (disk_number, source, auxiliar, destino)
+    ; [ebp+8] número de discos restantes na Torre de origem
+    ; [ebp+12] = Torre de origem
+    ; [ebp+16] = Torre de auxiliar
+    ; [ebp+20] = Torre de destino
+
+    push ebp                      ; empurra o registrador ebp na pilha
+    mov ebp, esp                  ; aponta o ponteiro do topo da pilha para a base
+
+    mov eax, [ebp+8]              ; move para o registrador ax o numero de discos na Torre de origem
+
+    cmp eax, 0                    ; verifica se Ainda há disco a ser movido na torre de origem
+    je end_procedure              ; caso nao tenha nunhum disco, pula finalizar o procedimento recursivo
+
+    ; Primeira chamada recursiva
+    push dword [ebp+16]           ; Empurra a torre Auxiliar
+    push dword [ebp+20]           ; Empurra a torre Destino
+    push dword [ebp+12]           ; Empurra a torre Origem
+
+    dec eax                       ; tira o disco do topo da torre de origem para ser colocado outra torre
+    push dword eax                ; empurra o numero de discos restantes a serem movidos na Torre de origem
+
+    call tower_of_hanoi
+
+    ; Printando os movimentos
+    push dword [ebp+16]           ; empilha o torre de Saida
+    push dword [ebp+12]           ; empilha o torre de Ida
+    push dword [ebp+8]            ; empilha o disco
+    call print_disk_movement
+
+    ; Segunda chamada recursiva
+    push dword [ebp+12]           ; Empurra a torre Origem
+    push dword [ebp+16]           ; Empurra a torre Auxiliar
+    push dword [ebp+20]           ; Empurra a torre Destino
+
+    mov eax, [ebp+8]              ; move para o registrador eax o número de discos restantes
+
+    dec eax                       ; tira o disco do topo da torre de origem para ser colocado outra torre
+    push dword eax                ; empurra o numero de discos restantes a serem movidos na Torre de origem
+
+    call tower_of_hanoi           ; chama a label tower_of_hanoi para guardar a linha de retorno (recursao)
+
+end_procedure:
+    mov esp, ebp
+    pop ebp
+    ret
+
+print_disk_movement:
+    push ebp                     ; empurra o registrador ebp na pilha (para ser a base)
+    mov ebp, esp                 ; aponta o ponteiro do topo da pilha (esp) para a base
+
+    mov eax, [ebp+8]             ; coloca no registrador eax o disco a ser movido
+    add al, 48                   ; conversão na tabela ASCII
+    mov [disk], al               ; coloca o valor em [disk] para o print
+
+    mov eax, [ebp+12]            ; coloca no registrador eax a torre de onde o disco saiu
+    add al, 64                   ; conversão na tabela ASCII
+    mov [tower_source], al       ; coloca o valor em [tower_source] para o print
+
+    mov eax, [ebp+16]            ; coloca no registrador eax a torre de onde o disco foi
+    add al, 64                   ; conversão na tabela ASCII
+    mov [tower_destination], al  ; coloca o valor em [tower_destination] para o print
+
+    mov edx, output_length
+    mov ecx, msg
+    mov ebx, STDOUT
+    mov eax, sys_write
+    int sys_call
+
+    call end_procedure
+
+exit:
+    mov eax, sys_exit
+    mov ebx, ret_exit
+    int sys_call
